50e9c408fa05f10e06d9424c3a83e433de8bef07
Michon van Dooren <michon1992@gmail.com>
2017-05-30 09:35:04 +0200

    Added locking/lockfile support

--- cron-reporter/__main__.py
+++ cron-reporter/__main__.py
@@ -2,8 +2,12 @@
 
 import argparse
 import datetime
+import os.path
 import subprocess
 import sys
+import tempfile
+
+import fasteners
 
 from timedeltatype import TimeDeltaType
 
@@ -17,6 +21,18 @@ parser.add_argument(
     help='the timeout of the command',
 )
 parser.add_argument(
+    '-l',
+    '--lock',
+    action='store_true',
+    default=False,
+    help='only allow one instance of this command to run at the same time',
+)
+parser.add_argument(
+    '--lockfile',
+    type=str,
+    help='the lockfile path to use (default: based on command)'
+)
+parser.add_argument(
     'command',
     nargs='+',
     help='the command to run',
@@ -25,6 +41,20 @@ parser.add_argument(
 # Parse the arguments
 args = parser.parse_args()
 print(args)
+
+# Lockfile
+if args.lock:
+    if not args.lockfile:
+        args.lockfile = os.path.join(
+            tempfile.gettempdir(),
+            '{}.lock'.format(args.command[0].split()[0])
+        )
+    lock = fasteners.InterProcessLock(args.lockfile)
+    if not lock.acquire(blocking=False):
+        print('could not acquire lock ({}), aborting'.format(args.lockfile))
+        sys.exit(1)
+elif args.lockfile:
+    parser.error('--lockfile requires --lock')
 
 # Run the command
 proc = subprocess.Popen(
